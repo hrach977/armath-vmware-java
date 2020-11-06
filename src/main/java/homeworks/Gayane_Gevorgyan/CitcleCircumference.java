9a06f26fc8c82876a30e2142b8848416a7709dbf
gaynane <47750898+gaynane@users.noreply.github.com>
2020-11-06 21:55:13 +0400

    Gayane

---
+++ src/main/java/homeworks/Gayane_Gevorgyan/CitcleCircumference.java
@@ -0,0 +1,10 @@
+package homeworks.Gayane_Gevorgyan
+
+        public class CircleCircumference {
+
+            public static void main(String[] args) {
+
+                System.out.println("Hello")
+            }
+
+        }
