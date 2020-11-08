a2c186b56cfc6e930d828b3a793091ea6e29ba46
gaynane <47750898+gaynane@users.noreply.github.com>
2020-11-08 18:43:56 +0400

    Homeworks

---
+++ src/main/java/homeworks/gayane/gevorgyan/EvenIndexesItems.java
@@ -0,0 +1,25 @@
+package homeworks.gayane.gevorgyan;
+import java.util.Scanner;
+
+public class EvenIndexesItems{
+
+    public static void main(String[] args){
+        System.out.println("Print items with even indexes...");
+        System.out.print("Please input the size of the array: ");
+        Scanner number = new Scanner(System.in);
+        int size = number.nextInt();
+        int array[] = new int[size];
+
+        System.out.print("Please input the integers: ");
+        for(int i = 0; i < size; ++i){
+            array[i] = number.nextInt();
+        }
+        System.out.print("Even indexes items: ");
+        for(int i = 0; i < array.length; ++i){
+            if(i % 2 == 0){
+                System.out.print(array[i] + ", ");
+            }
+        }
+
+    }
+}
