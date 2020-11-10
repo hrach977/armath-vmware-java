591e75dd3441a2d9c7a0cdffb2aee6a97e818052
Admin <arusikroman1111@gmail.com>
2020-11-11 00:00:44 +0400

    area

--- src/main/java/homeworks/arusik/apinyan/CircumferenceOfCircle.java
+++ src/main/java/homeworks/arusik/apinyan/CircumferenceOfCircle.java
@@ -22,7 +22,45 @@ public class CircumferenceOfCircle {
         return circumference;
     }
 
+    public static double radiusOfCircle() {
+        System.out.println("Radius of the circle");
+        System.out.println("Input area:");
+        Scanner scanner = new Scanner(System.in);
+        final double pi = 3.14;
+        double radius = 0;
+        while (scanner.hasNext()){
+            double area = scanner.nextDouble();
+            if (area < 0){
+                System.out.println("Area should not be a negative number!");
+            } else {
+                radius = Math.sqrt(area / pi);
+                System.out.println("Radius of the circle with area  " + area + " is " + radius);
+                break;
+            }
+        }
+        return radius;
+    }
+    public static double areaOfRectangle() {
+        System.out.println("Area of the rectangle");
+        System.out.println("Input side:");
+        Scanner scanner = new Scanner(System.in);
+        double area = 0;
+        double side = 0;
+        while (scanner.hasNext()) {
+            if (side < 0) {
+                System.out.println("Area should not be a negative number!");
+            } else {
+                side = scanner.nextDouble();
+                area = side * side;
+                System.out.println("Area of the rectangle with side" + side + "side = " + area);
+                break;
+            }
+        }
+        return area;
+
+    }
     public static void main(String[] args) {
-        circumferenceOfCircle();
+        //radiusOfCircle();
+        areaOfRectangle();
     }
 }
