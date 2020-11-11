85db50c2db425fdb49fc40a760af98b3534a517f
Admin <arusikroman1111@gmail.com>
2020-11-12 01:06:30 +0400

    array

--- src/main/java/homeworks/arusik/apinyan/CircumferenceOfCircle.java
+++ src/main/java/homeworks/arusik/apinyan/CircumferenceOfCircle.java
@@ -89,12 +89,57 @@ public static void sumOfDigits(){
         System.out.println("Summation of digits of  " + num + " is = " + sum);
 
 }
+//======================================================
+//6.	Write a program that prints all odd numbers in the array of integers.
+public static void printOddNumbers() {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Print odd numbers");
+    System.out.print("Input the size of array: ");
+    final int count = scanner.nextInt();
+
+    int[] sequence = new int[count];
+    int index = 0;
+    while (index < count && scanner.hasNext()) {
+        sequence[index] = scanner.nextInt();
+        index++;
+    }
+    int counter = 0;
+    for (int i = 0; i < sequence.length; ++i) {
+        System.out.print(sequence[i] + " ");
+        if (sequence[i] % 2 != 0) {
+            counter++;
+        }
+    }
+    System.out.println();
+    if (counter == 0){
+        System.out.println("There are no odd numbers in the array!");
+        return;
+    } else {
+        int[] sequence2 = new int[counter];
+        int j = 0;
+        for (int i = 0; i < sequence.length; ++i) {
+            if (sequence[i] % 2 != 0) {
+                sequence2[j] = sequence[i];
+                j++;
+            }
+        }
+
+        System.out.println("Odd numbers are: ");
+        for (int i = 0; i < counter; ++i) {
+            System.out.print(sequence2[i] + " ");
+        }
+    }
+    }
+
+
+
 
     public static void main(String[] args) {
         //circumferenceOfCircle();
         //radiusOfCircle();
         //areaOfRectangle();
         //swapNum1Num2();
-        sumOfDigits();
+        //sumOfDigits();
+            printOddNumbers();
     }
 }
