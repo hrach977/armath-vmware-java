790cbda5a002bd63dfaef5473f870b7d771e0680
hrach977 <hrach97@gmail.com>
2020-11-19 19:20:29 +0400

    checkpoint

---
+++ src/main/java/problem/solving/session3/oop/Circle.java
@@ -0,0 +1,25 @@
+package problem.solving.session3.oop;
+
+public class Circle {
+
+    private double radius;
+    private String color;
+
+    public Circle() {
+        radius = 1.0;
+        color = "red";
+    }
+
+    public Circle(double r) {
+        radius = r;
+        color = "red";
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public double getArea() {
+        return 1.0;
+    }
+}
