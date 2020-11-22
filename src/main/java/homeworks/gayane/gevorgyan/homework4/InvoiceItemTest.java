e81715d8edfc698017538aae91a1fb3caa6312e4
gaynane <47750898+gaynane@users.noreply.github.com>
2020-11-22 22:37:59 +0400

    Homeworks

---
+++ src/main/java/homeworks/gayane/gevorgyan/homework4/InvoiceItemTest.java
@@ -0,0 +1,14 @@
+package homeworks.gayane.gevorgyan.homework4;
+
+public class InvoiceItemTest {
+    public static void main(String[] args) {
+        InvoiceItem invoiceItem1 = new InvoiceItem("1", "Pen", 50, 100);
+        InvoiceItem invoiceItem2 = new InvoiceItem("2", "Notepad", 30, 300);
+
+
+        System.out.println("InvoiceItem [id = " + invoiceItem1.getId() + ",  decs = " + invoiceItem1.getDesc() + ", qty = " + invoiceItem1.getQty() + ", unitPrice = "+ invoiceItem1.getUnitPrice()+ "]" );
+        System.out.println("Total = " + invoiceItem1.getTotal());
+        System.out.println("InvoiceItem [id = " + invoiceItem2.getId() + ",  decs = " + invoiceItem2.getDesc() + ", qty = " + invoiceItem2.getQty() + ", unitPrice = "+ invoiceItem2.getUnitPrice()+ "]" );
+        System.out.println("Total = " + invoiceItem2.getTotal());
+    }
+}
