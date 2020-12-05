8ffbc61983ea2a4c1e314439d4f63dcde19fef88
Meline88 <meline.abrahamian@rambler.ru>
2020-12-05 13:30:33 +0400

    Warmup1 Meline

--- src/main/java/homeworks/meline/abrahamyan/WarmupOne.java
+++ src/main/java/homeworks/meline/abrahamyan/WarmupOne.java
@@ -111,6 +111,179 @@ public class WarmupOne {
         }
         return false;
     }
+//    Given a string, return a string made of the first 2 chars (if present),
+//    however include first char only if it is 'o' and include the second only if
+//    it is 'z', so "ozymandias" yields "oz".
+//
+//
+//    startOz("ozymandias") → "oz"
+//    startOz("bzoo") → "z"
+//    startOz("oxx") → "o"
+
+    public String startOz(String str) {
+        String st = "";
+        if (str.length() >= 1 && str.charAt(0)=='o') {
+            st = st + str.charAt(0);
+        }
+        if (str.length() >= 2 && str.charAt(1)=='z') {
+            st = st + str.charAt(1);
+        }
+
+        return st;
+    }
+
+//    Given three int values, a b c, return the largest.
+//
+//
+//    intMax(1, 2, 3) → 3
+//    intMax(1, 3, 2) → 3
+//    intMax(3, 2, 1) → 3
+
+    public int intMax(int a, int b, int c) {
+        int max;
+        if (a > b && a > c){
+            max = a;
+        }
+        if (a > b && a > c){
+            max = a;
+        } else if (b > c && b > a){
+            max = b;
+        } else {
+            max = c;
+        }
+        return max;
+    }
 
 
+//    Given 2 int values, return whichever value is nearest to the value 10,
+//    or return 0 in the event of a tie. Note that Math.abs(n) returns the absolute value of a number.
+//
+//
+//    close10(8, 13) → 8
+//    close10(13, 8) → 8
+//    close10(13, 7) → 0
+
+    public int close10(int a, int b) {
+        if (Math.abs(a-10) < Math.abs(b-10)) {
+            return a;
+        }
+        if (Math.abs(a-10) > Math.abs(b-10)) {
+            return b;
+        }
+        return 0;
+    }
+
+//    Given 2 int values, return true if they are both in the range 30..40 inclusive,
+//    or they are both in the range 40..50 inclusive.
+//
+//
+//    in3050(30, 31) → true
+//    in3050(30, 41) → false
+//    in3050(40, 50) → true
+
+    public boolean in3050(int a, int b) {
+        if ((a >= 30 && a <= 40 && b >= 30 && b <= 40)) {
+            return true;
+        }
+        if ((a >= 40 && a <= 50 && b >= 40 && b <= 50)) {
+            return true;
+        }
+        return false;
+    }
+
+
+//    Given 2 positive int values, return the larger value that is in the range 10..20 inclusive,
+//    or return 0 if neither is in that range.
+//
+//
+//    max1020(11, 19) → 19
+//    max1020(19, 11) → 19
+//    max1020(11, 9) → 11
+
+    public int max1020(int a, int b) {
+        if (a >= 10 && a <= 20 && b >= 10 && b <= 20){
+            if (a > b){
+                return a;
+            } else {
+                return b;
+            }
+        }
+        if (a >= 10 && a <= 20 && (b < 10 || b > 20)){
+            return a;
+        }
+        if (b >= 10 && b <= 20 && (a < 10 || a > 20)){
+            return b;
+        }
+        return 0;
+    }
+
+
+//    Return true if the given string contains between 1 and 3 'e' chars.
+//
+//
+//    stringE("Hello") → true
+//    stringE("Heelle") → true
+//    stringE("Heelele") → false
+  public boolean stringE(String str) {
+    int count = 0;
+    if (str.length()>=1) {
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i)=='e'){
+                count++;
+            }
+        }
+        if (count >= 1 && count <= 3){
+            return true;
+        }
+    }
+    return false;
+}
+
+//    Given two non-negative int values, return true if they have the same last digit,
+//    such as with 27 and 57. Note that the % "mod" operator computes remainders, so 17 % 10 is 7.
+//
+//
+//    lastDigit(7, 17) → true
+//    lastDigit(6, 17) → false
+//    lastDigit(3, 113) → true
+
+    public boolean lastDigit(int a, int b) {
+        return (a % 10 == b % 10);
+    }
+
+//    Given a string, return a new string where the last 3 chars are now in upper case.
+//    If the string has less than 3 chars, uppercase whatever is there.
+//    Note that str.toUpperCase() returns the uppercase version of a string.
+//
+//
+//    endUp("Hello") → "HeLLO"
+//    endUp("hi there") → "hi thERE"
+//    endUp("hi") → "HI"
+
+    public String endUp(String str) {
+        if (str.length() > 3) {
+            String st = str.substring(str.length()-3,str.length());
+            str = str.substring(0,str.length()-3);
+            return str + st.toUpperCase();
+        }
+        return str.toUpperCase();
+    }
+
+
+//    Given a non-empty string and an int N, return the string made starting with char 0,
+//    and then every Nth char of the string. So if N is 3,
+//    use char 0, 3, 6, ... and so on. N is 1 or more.
+//
+//
+//    everyNth("Miracle", 2) → "Mrce"
+//    everyNth("abcdefg", 2) → "aceg"
+//    everyNth("abcdefg", 3) → "adg"
+
+    public String everyNth(String str, int n) {
+        String st = "";
+        for (int i = 0; i < str.length(); i += n){
+            st += str.charAt(i);
+        }
+        return st;
+    }
 }
